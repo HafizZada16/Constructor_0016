37a7f331199c92e4f8439f690be85bf9b9f7c9f3
HafizZada16 <hafizzada16@gmail.com>
2024-05-14 18:22:23 +0700

    Add modifier public berisi 8 member variable

--- mbrStatikfunc.cpp
+++ mbrStatikfunc.cpp
@@ -5,5 +5,19 @@ using namespace std;
 class mahasiswa
 {
 private:
-    ststic int nim;
-}
+    static int nim;
+public:
+    int id;
+    string nama;
+
+    void setID();
+    void printAll();
+
+    static void setNim(int pNim) { nim = pNim; }
+    static int getNim() { return nim; }
+    mahasiswa(string pnama)
+    {
+        nama = pnama;
+        setID();
+    }
+};
